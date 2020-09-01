8f8a3ffd510df37740c16bcf31d1a6bc5a8bc0a4
bepul0 <bepul.cse.ru@gmail.com>
2020-09-01 18:25:07 +0600

    image upload process continue

--- server/controllers/movie-controller.js
+++ server/controllers/movie-controller.js
@@ -25,54 +25,6 @@ createMovie = (req,res) =>{
         })
     })
 }
-// updateMovie = async (req, res) => {
-//    const updatebody = req.body
-//     console.log(updatebody)
-// console.log(updatebody);
-//     if (!updatebody) {
-//         //console.log('body');
-
-//         return res.status(400).json({
-//             success: false,
-//             error: 'You must provide a body to update',
-//         })
-//     }
-//     //const movie=new Movie(updatebody)
-//     //console.log(movie)
-//     Movie.findOne({ _id: req.params.id }, (err, movie) => {
-//        // console.log('findOne success');
-//         console.log("ok  "+req.params.id)
-//         if (err) {
-//             //console.log('err');
-
-//             return res.status(404).json({
-//                 err,
-//                 msg: 'Movie not found!',
-//             })
-//         }
-//         console.log("movie "+movie)
-//         movie.name = updatebody.name
-//         movie.time = updatebody.time
-//         movie.rating = updatebody.rating
-//         console.log("movieup "+movie)
-        
-//         movie.save().then(() => {
-//                 return res.status(200).json({
-//                     success: true,
-//                     id: movie._id,
-//                     msg: 'Movie updated!',
-//                    // data: movies
-//                 })
-//             })
-//             .catch(error => {
-//                 //console.log('not found')
-//                 return res.status(405).json({
-//                     error,
-//                     msg: 'Movie not updated!',
-//                 })
-//             })
-//     })
-// }
 getMovie = async(req,res)=>{
    await Movie.find({}, (err,movies)=>{
        if (err) {
@@ -85,38 +37,9 @@ getMovie = async(req,res)=>{
    }).catch(err=>console.log(err))
 }
 
-// deleteMovie = async(req,res)=>{
-//     await Movie.findOneAndDelete({_id: req.params.id},(err,movies)=>{
-//         if (err) {
-//             return res.status(402).json({success: false, error: err})
-//         }
-        
-       
-//         return res.status(200).json({success: true, data: movies})
-//     }).catch(err=>console.log(err))
-
-    
-// }
-
-
-
-// getMovieById = async (req, res) => {
-//     await Movie.findOne({ _id: req.params.id }, (err, movies) => {
-//         if (err) {
-//             return res.status(400).json({ success: false, error: err })
-//         }
-//         //console.log('okk')
-//         return res.status(200).json({ success: true, data: movies })
-//     }).catch(err => console.log(err))
-// }
-
 
 
 module.exports = {
     createMovie,
     getMovie,
-    // deleteMovie,
-    // updateMovie,
-    // getMovieById,    
-
 }
--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -3,10 +3,6 @@ const MovieController = require('../controllers/movie-controller.js')
 const router = express.Router()
 
 router.post('/movierouter',MovieController.createMovie)
-// router.put('/movierouter/:id',MovieController.updateMovie)
-// router.delete('/movierouter/:id',MovieController.deleteMovie)
-
-// router.get('/movierouter/:id',MovieController.getMovieById)
 router.get('/getmovies',MovieController.getMovie)
 
 
